83dcb7eb41161897af9aedeced05ae2bcf656b69
Ellen Park <ellenpark781@gmail.com>
2020-01-20 21:05:44 -0500

    added styling to the 5 day weather cards

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ var searchedCities = ["London", "Barcelona"];
 //TODO: localstorage.setItemJSON.stringify("searchedCities", "city")
 
 function makeButtons(){
-$("#cityList").empty();
+$("#citysearch").empty();
 for(var i = 0; i < searchedCities.length; i++){
-    var prevCity = $("<p>").append(searchedCities[i]).addClass("searchedCity")
-    $("#cityList").append(prevCity)
+    var prevCity = $("<li>").append(searchedCities[i]).addClass("searchedCity list-group-item")
+    $("#citysearch").append(prevCity)
 }
 }
 
@@ -17,47 +17,46 @@ makeButtons();
 
 
 
-$('#button').click(function(){
-var city = $('#citySearch').val(); 
-if (city){
-    search(city)
-};
+$('#button-addon2').click(function(){
+    var city = $('#citySearch').val(); 
+    if (city){
+        search(city)
+    };
 });
 
 $(document).on("click", ".searchedCity", function(){
 var cityInput = $(this).text()
-search(cityInput)
+search(cityInput);
 })
 
 function search(city){
-if(searchedCities.indexOf(city) === -1){
-    searchedCities.push(city)
-// set item stringify into local storage
-}
-makeButtons();
-        console.log (city);
-$.ajax({
-    url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=93a011811ca9f461f8bf87ca0588f418", 
-    method: "GET",
+    if(searchedCities.indexOf(city) === -1){
+        searchedCities.push(city)
+    // set item stringify into local storage
     }
-)
-// append city name 
-.then(function(response) {  
-    $("#currentCity").empty();
-    
-    $('#cityName').text(response.name + " ("+ moment().subtract(10, 'days').calendar() + ")" )  
+    makeButtons();
+            console.log (city);
+    $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=93a011811ca9f461f8bf87ca0588f418", 
+        method: "GET",
+        }
+    )
+    // append city name 
+    .then(function(response) {  
+        $("#currentCity").empty();
+        
+        $('#cityName').text(response.name + " ("+ moment().subtract(10, 'days').calendar() + ")" )  
 
-    console.log (response)
-    var temp= response.main.temp
-    var humidity= response.main.humidity
-    $("<p>").text("Temperature: " + temp + "˚F");
-    $("<p>").text("Humidity: " + humidity);
-    var speed = $("<p>").text("Speed: " + response.wind.speed + " MPH");
-    $("#currentCity").append(temp, humidity, speed); 
+        console.log (response)
+        var humidity= response.main.humidity
+        var temp = $("<p>").text("Temperature: " + response.main.temp + "˚F");
+        humidity= $("<p>").text("Humidity: " + humidity);
+        var speed = $("<p>").text("Speed: " + response.wind.speed + " MPH");
+        $("#currentCity").append(temp, humidity, speed); 
 
-// Have a 5-Day Forecast showing date, current weather status, Temperature, and Humidity
-var lon = response.coord.lon;
-var lat = response.coord.lat;
+    // Have a 5-Day Forecast showing date, current weather status, Temperature, and Humidity
+    var lon = response.coord.lon;
+    var lat = response.coord.lat;
 
 $.ajax({
     url: "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&APPID=93a011811ca9f461f8bf87ca0588f418", 
@@ -65,18 +64,28 @@ $.ajax({
     })
     .then(function(response){
 
+    $(".card-group").empty();    
+        console.log(response)
     var indexToCheck = [6, 14, 22, 30, 38];
     for(var i = 0; i < indexToCheck.length; i++){
         
-      var fiveDay = response.main.indexToCheck[i];
-      console.log (fiveDay[0])
-        $("<p>").append(temp + "˚F");
-        $("<p>").append(humidity);
-        $("<p>").append(temp, humidity); 
-        $("#fiveDay").append(temp, humidity); 
-    
-         }
-     });
+    var fiveDay = response.list[indexToCheck[i]];
+    console.log (fiveDay)
+    var fiveTemp = $("<p>").append("Temperature: " + fiveDay.main.temp + "˚F").addClass("card-text");
+    var fiveHumidity = $("<p>").append("Humidity: " + fiveDay.main.humidity + "%").addClass("card-text");
+    // $("#fiveDay").append(temp, humidity); 
+    var date = moment(fiveDay.dt_txt)
+    console.log(date.format('MMMM Do YYYY, h:mm'))
+    var fiveDate = $("<h5>").addClass("card-title").append(date.format('MMMM Do YYYY, h:mm a'))
+
+    var cardBody = $("<div>").addClass("card-body");
+    var card = $("<div>").addClass("card text-white bg-primary mb-3");
+    cardBody.append(fiveDate, fiveTemp, fiveHumidity);
+    card.append(cardBody);
+    $(".card-group").append(card, "<div/>")
+
+        }
     });
- }
+    });
+    }
 });
